bbbcb3bc7d540411d8aeaaa66bea18a8c709c5d3
Lixing Ding <ding.lixing@gmail.com>
2017-03-15 07:42:17 +0800

    add base layer images

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
-chmod a+x env-build/cupkee_utils/*
+# env-build image build
+# docker build -t cupkee/env-base:1.00 ./env-base
 
 # env-build image build
-docker build -t env-build:0.07 ./env-build
+#chmod a+x env-build/cupkee_utils/*
+docker build -t cupkee/env-build:0.11 ./env-build
 
 # env-devel image build
 
