8339ff45f042f8aa920cfde9235f328439e48574
Lixing Ding <ding.lixing@gmail.com>
2017-03-15 12:29:32 +0800

    update docker file

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env sh
 
 # env-build image build
-# docker build -t cupkee/env-base:1.00 ./env-base
+docker build -t cupkee/env-base:1.00 ./env-base
 
 # env-build image build
 #chmod a+x env-build/cupkee_utils/*
-docker build -t cupkee/env-build:0.11 ./env-build
-
-# env-devel image build
+docker build -t cupkee/env-build:0.15 ./env-build
 
--- env-build/cupkee_docker_entrypoint.sh
+++ env-build/cupkee_docker_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-export PATH=$PATH:/usr/local/bin/gcc-arm-none-eabi-5_4-2016q3/bin:/usr/local/bin/cupkee_utils
+# export PATH=$PATH:/usr/local/bin/gcc-arm-none-eabi-5_4-2016q3/bin:/usr/local/bin/cupkee_utils
 
 exec $@
